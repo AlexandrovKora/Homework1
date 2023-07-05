a1b8d9f0233353e010ee5533dd5864b1f4eebdfc
Mishka <lp-fan1992@mail.ru>
2023-07-05 23:50:43 +0300

    task 8

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@
 // if (num1<num2) Console.Write(num2+" - наибольшее");
 // else Console.Write(num1+" - наибольшее");*/
 
-/* //Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.*/
+/* //Task 4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 Console.Write("Input the first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input the second number: ");
@@ -15,13 +15,21 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input the third number: ");
 int num3 = Convert.ToInt32(Console.ReadLine());
 
-while ((num1 > num2) && (num1 > num3)) {
-    Console.WriteLine(num1);
-    if ((num2 > num3) && (num2 > num1)) {
-        Console.WriteLine(num2);
-    }
-    else {
-        Console.WriteLine(num3);
-    }
-}
+int max = num1;
+
+if (num1 > max) max=num1;
+if (num2 > max) max=num2;
+if (num3 > max) max=num3;
+
+Console.Write("max="+max);*/
+
+/* //Task 6. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Write your number ");
+int num1=Convert.ToInt32(Console.ReadLine());
+if (num1%2==0) Console.Write("It is an even number");
+else Console.Write("It is an odd number");*/
+
+/* //Task 8. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.*/
+
 
